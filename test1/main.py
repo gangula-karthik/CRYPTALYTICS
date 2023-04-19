b7a9c0d124a977b1ff3e911dd6779cabeee53a4f
ngjunming8 <mrngjunming@gmail.com>
2023-04-19 11:49:29 +0800

    a

--- test1/main.py
+++ test1/main.py
@@ -9,7 +9,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("base.html")
 
 if __name__ == '__main__':
     socketio.run(app, host='localhost',allow_unsafe_werkzeug=True)
