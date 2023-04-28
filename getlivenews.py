f9e8d599ad6e00db7c9b831cf088703795863963
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-04-28 23:10:04 +0800

    add: code for retrieving crypto news articles-v1.3

---
+++ getlivenews.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+url = "https://newsapi.org/v2/everything?q=btc-usd&language=en&sortBy=publishedAt&apiKey=1b79f31f4909475bbfeadd33d9e08df3"
+
+response = requests.get(url)
+data = response.json()["articles"]
+x = 0
+for i in range(len(data)):
+    if "bitcoin" in data[i]["title"].lower():
+        print(str(x), str(data[i]["title"]))
+        x += 1
+    else: 
+        pass
