bc7957b08a3cdcf679c1ef83bbbe0f6b0cbc867e
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-05-03 15:23:06 +0800

    add: client side table filtering

--- main.py
+++ main.py
@@ -5,31 +5,18 @@ import json
 
 app = Flask(__name__)
 
-def get_news(search_query=None):
-    url = f"https://newsapi.org/v2/everything?q={search_query}&language=en&sortBy=publishedAt&apiKey=1b79f31f4909475bbfeadd33d9e08df3"
-    response = requests.get(url)
-    data = response.json()["articles"]
-    x = 0
-    news = []
-    for i in range(len(data)):
-        if search_query:
-            if search_query.lower() in data[i]["title"].lower():
-                news.append(data[i])
-                x += 1
-    return news
 
 
-
+def get_price():
+    url = "https://api.binance.com/api/v3/ticker/24hr"
+    response = requests.get(url).json()
+    return response
 
 @app.route('/pricechange', methods=['GET'])
 def dayPriceChange(): 
-    url = "https://api.binance.com/api/v3/ticker/24hr"
-    response = requests.get(url).json()
+    response = get_price()
     topcrypto = top3crypto()
     return render_template('pricechange.html', data=response, top=topcrypto)
-
-
-
 
 def top3crypto():
     url = 'https://api1.binance.com/api/v3/ticker/24hr'
@@ -41,6 +28,18 @@ def top3crypto():
 
 
 
+def get_news(search_query):
+    url = f"https://newsapi.org/v2/everything?q={search_query}&language=en&sortBy=publishedAt&apiKey=1b79f31f4909475bbfeadd33d9e08df3"
+    response = requests.get(url)
+    data = response.json()["articles"]
+    x = 0
+    news = []
+    for i in range(len(data)):
+        if search_query:
+            if search_query.lower() in data[i]["title"].lower():
+                news.append(data[i])
+                x += 1
+    return news
 
 @app.route('/cryptonews', methods=['GET'])
 def news():
