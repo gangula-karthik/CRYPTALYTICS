3fd01d4f1e2ab8f3508c6ce53ffa57e88a9e39f9
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-05-03 12:01:17 +0800

    add: search bar

--- getlivenews.py
+++ getlivenews.py
@@ -1,25 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
 
 app = Flask(__name__)
 
-def get_news():
+def get_news(search_query=None):
     url = "https://newsapi.org/v2/everything?q=btc-usd&language=en&sortBy=publishedAt&apiKey=1b79f31f4909475bbfeadd33d9e08df3"
     response = requests.get(url)
     data = response.json()["articles"]
     x = 0
     news = []
     for i in range(len(data)):
-        if "bitcoin" in data[i]["title"].lower():
+        if search_query:
+            if search_query.lower() in data[i]["title"].lower():
+                news.append(data[i])
+                x += 1
+        elif "bitcoin" in data[i]["title"].lower():
             news.append(data[i])
             x += 1
     return news
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    news_data = get_news()
+    search_query = request.args.get('search')
+    news_data = get_news(search_query)
     return render_template('news.html', news=news_data) 
 
 if __name__ == '__main__':
