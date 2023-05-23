3e997533d8494972b38924c458aba311eb051010
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-05-23 22:39:54 +0800

    most likely my final commit 👍

--- main.py
+++ main.py
@@ -155,13 +155,8 @@ def gptcrypto():
         return render_template('cryptogpt.html')
 
 
-
-
-
-
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
 @app.route('/')
